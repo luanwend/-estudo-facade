61dd23c8967e222aefc77f60fd4cc5bff98ddac8
luanwend <luanwendling65@hotmail.com>
2022-07-13 15:31:11 -0300

    estudo facade

---
+++ README.md
@@ -0,0 +1,3 @@
+# Explorando Padrões de Projetos na Prática com Java
+
+implementações do padrao de projeto Facade explorado no Lab "Explorando Padrões de Projetos na Prática com Java".
---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Venilton", "14801788");
+	}
+
+}
